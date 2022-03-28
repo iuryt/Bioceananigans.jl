1781af059eb2cecd111c767c1ff6b14a2c05ca98
iury simoes-sousa <simoesiury@gmail.com>
2022-03-28 11:03:31 -0400

    fixed the problem with varying total biomass

--- NP_test.jl
+++ NP_test.jl
@@ -2,13 +2,13 @@ using Oceananigans
 using Oceananigans.Units
 
 # define the size and max depth of the simulation
-const Nx = 5
-const Ny = 5
+const Nx = 2
+const Ny = 2
 const Nz = 48 # number of points in z
 const H = 1000 # maximum depth
 
 # create the grid of the model
-grid = RectilinearGrid(CPU(),
+grid = RectilinearGrid(GPU(),
     size=(Nx,Ny,Nz),
     x=(-Nx/2,Nx/2),
     y=(-Ny/2,Ny/2),
@@ -35,19 +35,20 @@ h = Field{Center, Center, Nothing}(grid)
 #light = Field{Center, Center, Center}(grid)
 #@inline light_growth(light) = (light*α)/sqrt(μ₀^2 + (light*α)^2)
 
+const L0 = 100
 # evolution of the available light at the surface
-@inline L0(t) = t/10 + 14
+@inline L(z) = L0*exp.(z*Kw)
 # light profile
-@inline light_growth(z,t) = (L0(t)*exp.(z*Kw)*α)/sqrt(μ₀^2 + (L0(t)*exp.(z*Kw)*α)^2)
+@inline light_growth(z,t) = μ₀ * (L(z)*α)/sqrt(μ₀^2 + (L(z)*α)^2)
 
 # nitrate and ammonium limiting
-@inline N_lim(N,Nr) = N/(N+kn) * kr/(Nr+kr)
-@inline Nr_lim(Nr) = Nr/(Nr+kr)
+@inline N_lim(N,Nr) = (N/(N+kn)) * (kr/(Nr+kr))
+@inline Nr_lim(Nr) =  (Nr/(Nr+kr))
 
 # functions for the NP model
-P_forcing(x, y, z, t, P, N, Nr)  =   μ₀ * light_growth(z,t) * (N_lim(N,Nr) + Nr_lim(Nr)) * P - m * P^2
-N_forcing(x, y, z, t, P, N, Nr)  = - μ₀ * light_growth(z,t) * N_lim(N,Nr) * P
-Nr_forcing(x, y, z, t, P, N, Nr) = - μ₀ * light_growth(z,t) * N_lim(N,Nr) * P + m * P^2
+P_forcing(x, y, z, t, P, N, Nr)  =   light_growth(z,t) * (N_lim(N,Nr) + Nr_lim(Nr)) * P - m * P^2
+N_forcing(x, y, z, t, P, N, Nr)  = - light_growth(z,t) * N_lim(N,Nr) * P
+Nr_forcing(x, y, z, t, P, N, Nr) = - light_growth(z,t) * Nr_lim(Nr) * P + m * P^2
 
 # using the functions to determine the forcing
 P_dynamics = Forcing(P_forcing, field_dependencies = (:P,:N,:Nr))
@@ -77,7 +78,7 @@ const ρₒ = 1026
 set!(model;b=B,P=P,N=13,Nr=0)
 
 # create a simulation
-simulation = Simulation(model, Δt = 1hour, stop_time = 50days)
+simulation = Simulation(model, Δt = 1minutes, stop_time = 20days)
 
 # function to compute the mld
 function compute_mixed_layer_depth!(sim)
@@ -140,8 +141,28 @@ end
 # writing the output
 simulation.output_writers[:fields] =
     NetCDFOutputWriter(model, merge(model.velocities, model.tracers), filepath = "data/NP_output.nc",
-                     schedule=TimeInterval(1minute))
+                     schedule=TimeInterval(1hour))
 
+
+using Printf
+
+function print_progress(simulation)
+    b, P, N, Nr = simulation.model.tracers
+
+    # Print a progress message
+    msg = @sprintf("i: %04d, t: %s, Δt: %s, P_max = %.1e, N_min = %.1e, Nr_max = %.1e, wall time: %s\n",
+                   iteration(simulation),
+                   prettytime(time(simulation)),
+                   prettytime(simulation.Δt),
+                   maximum(P), minimum(N), maximum(Nr),
+                   prettytime(simulation.run_wall_time))
+
+    @info msg
+
+    return nothing
+end
+
+simulation.callbacks[:progress] = Callback(print_progress, TimeInterval(1hour))
 
 # run the simulation
 run!(simulation)
