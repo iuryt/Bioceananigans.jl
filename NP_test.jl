9a7ae64f8b33be58870c8580ccf99595f7bb413e
iury simoes-sousa <simoesiury@gmail.com>
2022-03-31 19:22:54 -0400

    cleaned code, add light, adaptive timestep and zero negative N values

--- NP_test.jl
+++ NP_test.jl
@@ -2,73 +2,90 @@ using Oceananigans
 using Oceananigans.Units
 
 # define the size and max depth of the simulation
+const sponge = 20
 const Ny = 100
 const Nz = 48 # number of points in z
 const H = 1000 # maximum depth
 
 # create the grid of the model
-grid = RectilinearGrid(GPU(),
-    size=(Ny,Nz),
-    y=(-(Ny/2)kilometers,(Ny/2)kilometers),
+grid = RectilinearGrid(CPU(),
+    size=(Ny+2sponge, Nz),
+    halo=(3,3),
+    y=(-(Ny/2 + sponge)kilometers, (Ny/2 + sponge)kilometers), 
     z=(H * cos.(LinRange(π/2,0,Nz+1)) .- H)meters,
-    topology=(Flat, Periodic, Bounded)
+    topology=(Flat, Bounded, Bounded)
 )
 
-# define the turbulence closure of the model
-horizontal_closure = ScalarDiffusivity(ν=1, κ=1)
-vertical_closure = ScalarDiffusivity(ν=1e-5, κ=1e-5)
+
+coriolis = FPlane(latitude=60)
+
+@inline νh(x,y,z,t) = ifelse((y>-(Ny/2)kilometers)&(y<(Ny/2)kilometers), 1, 100)
+horizontal_closure = HorizontalScalarDiffusivity(ν=νh, κ=νh)
+
+@inline νz(x,y,z,t) = ifelse((y>-(Ny/2)kilometers)&(y<(Ny/2)kilometers), 1e-5, 1e-3)
+vertical_closure = ScalarDiffusivity(ν=νz, κ=νz)
+
+
+#--------------- NP Model
 
 # constants for the NP model
 const μ₀ = 1/day   # surface growth rate
 const m = 0.015/day # mortality rate due to virus and zooplankton grazing
-
 const Kw = 0.059 # meter^-1
 const Kc = 0.041 # m^2 mg^-1
 const kn = 0.75
 const kr = 0.5
 const α = 0.0538/day
+const L0 = 100
+
 
 # create the mld field that will be updated at every timestep
 h = Field{Center, Center, Nothing}(grid) 
 light = Field{Center, Center, Center}(grid)
 
-const L0 = 100
 # evolution of the available light at the surface
 @inline L(z) = L0*exp.(z*Kw)
 # light profile
 @inline light_growth(z) = μ₀ * (L(z)*α)/sqrt(μ₀^2 + (L(z)*α)^2)
 
 # nitrate and ammonium limiting
-@inline N_lim(N,Nr) = (N/(N+kn)) * (kr/(Nr+kr))
+@inline N_lim(N, Nr) = (N/(N+kn)) * (kr/(Nr+kr))
 @inline Nr_lim(Nr) =  (Nr/(Nr+kr))
 
 # functions for the NP model
-P_forcing(x, y, z, t, P, N, Nr)  =   light * (N_lim(N,Nr) + Nr_lim(Nr)) * P - m * P^2
-N_forcing(x, y, z, t, P, N, Nr)  = - light * N_lim(N,Nr) * P
-Nr_forcing(x, y, z, t, P, N, Nr) = - light * Nr_lim(Nr) * P + m * P^2
+@inline P_forcing(light, P, N, Nr)  =   light * (N_lim(N, Nr) + Nr_lim(Nr)) * P - m * P^2
+@inline N_forcing(light, P, N, Nr)  = - light * N_lim(N, Nr) * P
+@inline Nr_forcing(light, P, N, Nr) = - light * Nr_lim(Nr) * P + m * P^2
+
+# functions for the NP model
+@inline P_forcing(i, j, k, grid, clock, fields, p)  = @inbounds P_forcing(p.light[i, j, k], fields.P[i, j, k], fields.N[i, j, k], fields.Nr[i, j, k])
+@inline N_forcing(i, j, k, grid, clock, fields, p)  = @inbounds N_forcing(p.light[i, j, k], fields.P[i, j, k], fields.N[i, j, k], fields.Nr[i, j, k])
+@inline Nr_forcing(i, j, k, grid, clock, fields, p) = @inbounds Nr_forcing(p.light[i, j, k], fields.P[i, j, k], fields.N[i, j, k], fields.Nr[i, j, k])
 
 # using the functions to determine the forcing
-P_dynamics = Forcing(P_forcing, field_dependencies = (:P,:N,:Nr))
-N_dynamics = Forcing(N_forcing, field_dependencies = (:P,:N,:Nr))
-Nr_dynamics = Forcing(Nr_forcing, field_dependencies = (:P,:N,:Nr))
+P_dynamics = Forcing(P_forcing, discrete_form=true, parameters=(; light))
+N_dynamics = Forcing(N_forcing, discrete_form=true, parameters=(; light))
+Nr_dynamics = Forcing(Nr_forcing, discrete_form=true, parameters=(; light))
 
-
-coriolis = FPlane(latitude=60)
+#--------------- Instantiate Model
 
 # create the model
 model = NonhydrostaticModel(grid = grid,
+                            advection = WENO5(),
+                            timestepper = :RungeKutta3,
                             coriolis = coriolis,
-                            closure=(horizontal_closure,vertical_closure),
+                            closure=(horizontal_closure, vertical_closure),
                             tracers = (:b, :P, :N, :Nr),
                             buoyancy = BuoyancyTracer(),
-                            forcing = (P=P_dynamics,N=N_dynamics,Nr=Nr_dynamics))
+                            forcing = (P=P_dynamics, N=N_dynamics, Nr=Nr_dynamics))
 
-# mld
-const cz = -200
-# gravity
-const g = 9.82
-# reference density
-const ρₒ = 1026
+
+
+#--------------- Initial Conditions
+
+const cz = -200 # mld
+const g = 9.82 # gravity
+const ρₒ = 1026 # reference density
 
 
 # background density profile based on Argo data
@@ -78,24 +95,23 @@ const ρₒ = 1026
 @inline decay(z) = (tanh((z+500)/300)+1)/2
 
 @inline front(x, y, z, cy) = tanh((y-cy)/12kilometers)
-@inline D(x, y, z) = bg(z) + 0.8*decay(z)*front(x,y,z,0)/4
-@inline B(x, y, z) = -(g/ρₒ)*D(x,y,z)
+@inline D(x, y, z) = bg(z) + 0.8*decay(z)*front(x, y, z, 0)/4
+@inline B(x, y, z) = -(g/ρₒ)*D(x, y, z)
 
 # initial phytoplankton profile
-@inline P(x, y, z) = ifelse(z>cz,0.4,0)
+@inline P(x, y, z) = ifelse(z>cz, 0.4, 0)
 
 # setting the initial conditions
-set!(model;b=B,P=P,N=13,Nr=0)
+set!(model; b=B, P=P, N=13, Nr=0)
 
 
-simulation = Simulation(model, Δt = 1minutes, stop_time = 2minutes)
-
+#--------------- Simulation
 
 # # create a simulation
-# simulation = Simulation(model, Δt = 1minutes, stop_time = 20days)
+simulation = Simulation(model, Δt = 1minutes, stop_time = 20days)
 
-# wizard = TimeStepWizard(cfl=1.0, max_change=1.1, max_Δt=6minutes)
-# simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(10))
+wizard = TimeStepWizard(cfl=1.0, max_change=1.1, max_Δt=1hour)
+simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(10))
 
 include("src/compute_mixed_layer_depth.jl")
 const Δb=(g/ρₒ) * 0.03
@@ -108,12 +124,17 @@ compute_light!(simulation) = compute_light!(light, h, simulation.model.tracers.P
 # add the function to the callbacks of the simulation
 simulation.callbacks[:compute_light] = Callback(compute_light!)
 
+# zero_tracer(sim) = map!(c -> ifelse(c < 0, 0.0, c), parent(sim.model.tracers.N), parent(sim.model.tracers.N))
+zero_N(sim) = parent(sim.model.tracers.N) .= max.(0, parent(sim.model.tracers.N))
+simulation.callbacks[:zero_N] = Callback(zero_N)
+
 # merge light and h to the outputs
 outputs = merge(model.velocities, model.tracers, (; light, h)) # make a NamedTuple with all outputs
+
 # writing the output
 simulation.output_writers[:fields] =
     NetCDFOutputWriter(model, outputs, filepath = "data/NP_output.nc",
-                     schedule=TimeInterval(1hour))
+                     schedule=TimeInterval(3hours))
 
 using Printf
 
@@ -127,14 +148,13 @@ function print_progress(simulation)
                    prettytime(simulation.Δt),
                    maximum(P), minimum(N), maximum(Nr),
                    prettytime(simulation.run_wall_time))
-
+    
     @info msg
 
     return nothing
 end
 
-# simulation.callbacks[:progress] = Callback(print_progress, TimeInterval(1hour))
-simulation.callbacks[:progress] = Callback(print_progress, IterationInterval(1))
+simulation.callbacks[:progress] = Callback(print_progress, TimeInterval(1hour))
 
 # run the simulation
 run!(simulation)
