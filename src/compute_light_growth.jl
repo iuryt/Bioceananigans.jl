b838659b3f837b12097bd378ff6484c18b82e6b0
iury simoes-sousa <simoesiury@gmail.com>
2022-04-02 18:46:48 -0400

    change name and set option for which average will be computed

--- src/compute_light_growth.jl
+++ src/compute_light_growth.jl
@@ -5,7 +5,7 @@ using Oceananigans.Utils: launch!
 using Oceananigans.Grids
 using Oceananigans.Operators: Δzᶜᶜᶜ
 
-@kernel function _compute_light!(light, grid, h, P, light_function, light_growth)
+@kernel function _compute_light_growth!(light, grid, h, P, light_function, light_growth_function, average_growth)
     i, j = @index(Global, NTuple)
 
     chlinteg = 0
@@ -13,7 +13,13 @@ using Oceananigans.Operators: Δzᶜᶜᶜ
     @unroll for k in grid.Nz : -1 : 1
         chlinteg = chlinteg + @inbounds P[i, j, k] * chl2c * Δzᶜᶜᶜ(i, j, k, grid)
         z_center = znode(Center(), Center(), Center(), i, j, k, grid)
-        @inbounds light[i, j, k] = light_growth(light_function(z_center)/exp(chlinteg*Kc))
+        
+        if average_growth
+            @inbounds light[i, j, k] = light_growth_function(light_function(z_center)/exp(chlinteg*Kc))
+        else
+            @inbounds light[i, j, k] = light_function(z_center)/exp(chlinteg*Kc)
+        end
+        
     end
 
     light_sum = 0
@@ -43,14 +49,20 @@ using Oceananigans.Operators: Δzᶜᶜᶜ
             @inbounds light[i, j, k] = light_sum/dz_sum     
         end
     end
+    
+    if ~(average_growth)
+        @unroll for k in grid.Nz : -1 : 1
+            @inbounds light[i, j, k] = light_growth_function(light[i, j, k])
+        end
+    end
 end
 
-function compute_light!(light, h, P, light_function, light_growth)
+function compute_light_growth!(light, h, P, light_function, light_growth_function, average_growth=false)
     grid = h.grid
     arch = architecture(grid)
 
     event = launch!(arch, grid, :xy,
-                    _compute_light!, light, grid, h, P, light_function, light_growth,
+                    _compute_light_growth!, light, grid, h, P, light_function, light_growth_function, average_growth,
                     dependencies = device_event(arch))
 
     wait(device_event(arch), event)
