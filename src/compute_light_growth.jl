8bdf410393efd23e128dfc79ee7363a348fec3c8
iury simoes-sousa <simoesiury@gmail.com>
2022-04-07 14:39:33 -0400

    passing average_code instead of the symbol (making it work on GPU)

--- src/compute_light_growth.jl
+++ src/compute_light_growth.jl
@@ -5,7 +5,7 @@ using Oceananigans.Utils: launch!
 using Oceananigans.Grids
 using Oceananigans.Operators: Δzᶜᶜᶜ
 
-@kernel function _compute_light_growth!(light, grid, h, P, light_function, light_growth_function, average, shading)
+@kernel function _compute_light_growth!(light, grid, h, P, light_function, light_growth_function, average_code, shading)
     i, j = @index(Global, NTuple)
 
     chlinteg = 0
@@ -16,17 +16,15 @@ using Oceananigans.Operators: Δzᶜᶜᶜ
         end
         z_center = znode(Center(), Center(), Center(), i, j, k, grid)
         
-        if average==:growth
+        if average_code==1
             @inbounds light[i, j, k] = light_growth_function(light_function(z_center)/exp(chlinteg*Kc))
-        elseif (average==nothing)|(average==:light)
+        elseif (average_code==0)|(average_code==2)
             @inbounds light[i, j, k] = light_function(z_center)/exp(chlinteg*Kc)
-        else
-            error("Unsupported average ($average).")
         end
         
     end
 
-    if ~(average==nothing)
+    if ~(average_code==0)
         light_sum = 0
         dz_sum = 0
 
@@ -56,7 +54,7 @@ using Oceananigans.Operators: Δzᶜᶜᶜ
         end
     end
     
-    if average!=:growth
+    if average_code!=:1
         @unroll for k in grid.Nz : -1 : 1
             @inbounds light[i, j, k] = light_growth_function(light[i, j, k])
         end
@@ -67,8 +65,19 @@ function compute_light_growth!(light, h, P, light_function, light_growth_functio
     grid = h.grid
     arch = architecture(grid)
 
+    
+    if average==nothing
+        average_code = 0
+    elseif average==:growth
+        average_code = 1
+    elseif average==:light
+        average_code = 2
+    else
+        error("Unsupported average ($average).")
+    end
+    
     event = launch!(arch, grid, :xy,
-                    _compute_light_growth!, light, grid, h, P, light_function, light_growth_function, average, shading,
+                    _compute_light_growth!, light, grid, h, P, light_function, light_growth_function, average_code, shading,
                     dependencies = device_event(arch))
 
     wait(device_event(arch), event)
